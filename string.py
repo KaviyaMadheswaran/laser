d54def1c488408e0b3b191216a30292c870568af
KaviyaMadheswaran <58587830+KaviyaMadheswaran@users.noreply.github.com>
2020-05-25 21:38:58 +0530

    Create string.py

---
+++ string.py
@@ -0,0 +1,6 @@
+a="mahendra singDhoni"
+b="sona college"
+print(a[0:3]);#print first 3 character
+print(a[5]);#print 5th character           
+print(a*2);#print the string twice
+print(a+" "+b);#concatenate the two string
