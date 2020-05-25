fe81c67ddb417f9e5d4a898835aa92daba84cdd9
KaviyaMadheswaran <58587830+KaviyaMadheswaran@users.noreply.github.com>
2020-05-25 21:31:51 +0530

    Create ifelse.py

---
+++ ifelse.py
@@ -0,0 +1,9 @@
+n=int(input("enter n?"))
+if n%2!=0:  
+  print("Weird")
+if n>=2 and n<=5 and n%2==0:  
+  print("Not Weird")
+if n>=6 and n<=20 and n%2==0:  
+  print("Weird")
+if n>20 and n%2==0:  
+  print("Not Weird")
