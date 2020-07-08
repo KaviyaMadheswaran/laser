ed65463e02c6bea2c82bca19df924c2585f87238
KaviyaMadheswaran <58587830+KaviyaMadheswaran@users.noreply.github.com>
2020-07-08 15:03:57 +0530

    Create even num in rev order.py

---
+++ even num in rev order.py
@@ -0,0 +1,7 @@
+#print 1 to n numbers
+'''n=int(input("enter number"))for i in range(1,n): if(i%2==0): print(i)'''
+#print even n umbers from 1 to n numbers in reverse order
+n=int(input("enter num"))
+for i in range(n,1,-1):
+   if(i%2==0):
+      print(i)
